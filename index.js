5e877c48b11dda7e890db5168c1d4d90950e09d5
Jeremy Wood <js.wood206@gmail.com>
2021-02-24 21:35:23 -0800

    commit tree e00655e5223e45c49032a6515d84953f8ca72da8

--- index.js
+++ index.js
@@ -9,5 +9,17 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(timeString) {
+  let tString = parseInt(timeString.split(":").join(""))
+  if (tString <= 1200) {
+    return "Good Morning"
+  } else if (tString <= 1700) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening";
+  }
+}
+
+function displayMessage(testContent) {
+  document.getElementById('greeting').innerText = testContent
+}
